1981d8761e89a632afa31f018073d5a19a3fa0d3
Adam Parker <adam@Opss-Mac-mini-2.local>
2016-11-22 15:30:42 +0000

    Added PHP to setup my mailing system

--- portfolio/email.php
+++ portfolio/email.php
@@ -1,12 +1,18 @@
 <?php
-  function email($first_name, $last_name, $email, $phone, $message){
-    $to = "adam3692@hotmail.co.uk";
-    $subject = "Website enquirey";
-    $message = $message;
-    $headers = 'From: webmaster@example.com' . "\r\n" .
-      'Reply-To: webmaster@example.com' . "\r\n" .
-      'X-Mailer: PHP/' . phpversion();
+  if(count($_POST) > 0) {
+    $first_name = $_POST['first_name'];
+    $last_name  = $_POST['last_name'];
+    $email      = $_POST['email'];
+    $phone      = $_POST['phone'];
+    $message    = $_POST['message'];
+    $header     = "Content-type: text/html\r\nReply-To: $email\r\nFrom: $first_name <$email>";
 
-      mail($to, $subject, $message, $headers);
+    $body ="Email recieved from ".$first_name." ".$last_name."<br /><hr />containing the following message: <br /> <div align='center'>".$message."</div><br /><hr />The following details were attached<br /><b>Email</b>: ".$email."<br /><b>Phone</b>: ".$phone;
+
+    if(mail("adam3692@hotmail.co.uk", "Website enquiry", $body, $header)) {
+      die("true");
+    } else {
+      die("There was an error sending the email!");
+    }
   }
 ?>
--- portfolio/home.js
+++ portfolio/home.js
@@ -27,18 +27,44 @@ $("#project-button").click(function(){
   }
 });
 
+function setText( text ){
+  $("#email-send").html(text);
+}
+
+  var isSubmitting = false;
 $("#email-send").click(function() {
-  $.ajax({
-    type: "POST",
-    url: "http://0.0.0.0:8000/email.php",
-    data: { first_name: $("#first-name").val(),
-            last_name:  $("#last-name").val(),
-            email:      $("#email").val(),
-            phone:      $("#phone").val(),
-            message:    $("#message").val() }
-  }).done(function( msg ) {
-    alert( "Data saved: " + msg );
-  });
+
+if( isSubmitting == false ){
+  isSubmitting = true;
+  var firstName = $("#first-name").val();
+  var lastName  = $("#last-name").val();
+  var email     = $("#email").val();
+  var phone     = $("#phone").val();
+  var message   = $("#message").val();
+
+  if( firstName == "" || lastName == "" || email == "" || phone == "" )
+    alert("Contact form not complete");
+  else {
+    setText("Sending...");
+    $.post("email.php", {
+      first_name: firstName, last_name: lastName, email: email, phone: phone, message: message
+    }, function(data) {
+      if(data == "true"){
+        setText("Sent!");
+      } else {
+        setText("Send mail");
+        alert(data);
+      }
+    });
+  }
+} else {
+  alert("You can only send 1 email at a time!");
+}
+
 });
+
+
+
+
 
 });
